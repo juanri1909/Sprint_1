8e90fa7b1e9eff287665b8e4eb675a82f707f601
juanri1909 <jrigarci1909@gmail.com>
2021-12-14 16:40:59 +0100

    Update n2Exercici1.java

--- S1.02/src/n2Exercici1/n2Exercici1.java
+++ S1.02/src/n2Exercici1/n2Exercici1.java
@@ -5,7 +5,6 @@ Proporciona al constructor de l'excepció un missatge especific.
 Captura l'excepció dins d'una clàusula catch i imprimeix el seu missatge. 
 Afegeix una clàusula finally i imprimeix un missatge per a demostrar 
 que va passar per allí.
-https://jarroba.com/excepciones-exception-en-java-con-ejemplos/
 */
 public class n2Exercici1 {
 
